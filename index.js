7bf16e51aab63b029193a354478173fa606b357f
miguel-22z2 <miguelalessilenzi@icloud.com>
2026-05-09 13:00:52 -0400

    atualizacao

---
+++ index.js
@@ -0,0 +1,6 @@
+if (window.confirm("Você vai em contratar?")) {
+    window.alert("Você faz a escolha certa!");
+} else {
+    window.alert("Você fez a escolha errada!");
+    window.location.href = "https://br.pinterest.com/search/pins/?q=gato%20mostrando%20o%20dedo%20do%20meio&rs=typed";
+}
